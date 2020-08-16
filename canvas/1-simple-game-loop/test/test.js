f80cb8e6520e31f841b0fe55c1fd7b6d26d9b4a5
Stephan Ljungros <stephan.ljungros@gmail.com>
2020-08-16 22:26:04 +0200

    Added validation and tests for those validations for vector.js.

--- canvas/1-simple-game-loop/test/test.js
+++ canvas/1-simple-game-loop/test/test.js
@@ -12,37 +12,74 @@ describe('Vectors', function () {
         vectorB.pos.y = d;
         done();
     });
+    describe('Successful tests', function () {
+        it('should add two vectors', function () {
+            vectorA.add(vectorB);
+            assert.strictEqual(vectorA.pos.x, 4);
+            assert.strictEqual(vectorA.pos.y, 6);
+        });
+        it('should subtract two vectors', function () {
+            vectorA.subtract(vectorB);
+            assert.strictEqual(vectorA.pos.x, -2);
+            assert.strictEqual(vectorA.pos.y, -2);
+        });
 
-   it('should add two vectors', function () {
-       vectorA.add(vectorB);
-       assert.strictEqual(vectorA.pos.x, 4);
-       assert.strictEqual(vectorA.pos.y, 6);
-   });
-   it('should subtract two vectors', function () {
-        vectorA.subtract(vectorB);
-        assert.strictEqual(vectorA.pos.x, -2);
-        assert.strictEqual(vectorA.pos.y, -2);
-   });
+        it('should return Vector.pos that\'s two times larger', function () {
+            vectorA.scalar(5);
+            assert.strictEqual(vectorA.pos.x, 5);
+            assert.strictEqual(vectorA.pos.y, 10);
+        });
+        it('return vector length', function () {
+            let length = vectorA.magnitude();
+            assert.strictEqual(length, 2.23606797749979);
+        });
 
-    it('should return Vector.pos that\'s two times larger', function () {
-        vectorA.scalar(5);
-        assert.strictEqual(vectorA.pos.x, 5);
-        assert.strictEqual(vectorA.pos.y, 10);
+        it('return magnitude between two vectors', function () {
+            let magnitude = vectorA.distance(vectorB);
+            assert.strictEqual(magnitude, 2.8284271247461903);
+        });
+
+        it('normalize a vector', function () {
+            vectorA.normalize();
+            let length = vectorA.magnitude();
+            assert.strictEqual(length, 0.9999999999999999);
+        });
+
+        it('should be an angle', function () {
+            let degrees = vectorA.directionAngle();
+            assert.strictEqual(Math.round(degrees), 63);
+        });
     });
-    it('return vector length', function () {
-       let length = vectorA.magnitude();
-       assert.strictEqual(length, 2.23606797749979);
-    });
 
-    it('return magnitude between two vectors', function () {
-       let magnitude = vectorA.distance(vectorB);
-       assert.strictEqual(magnitude, 2.8284271247461903);
-    });
-
-    it('normalize a vector', function () {
-       vectorA.normalize();
-       let length = vectorA.magnitude();
-       assert.strictEqual(length, 0.9999999999999999);
+    describe('Tests to fail', function () {
+        it('should fail to construct with non numbers', function () {
+            assert.throws(() => new Vector("h", "a"), TypeError);
+        });
+        it('should fail to construct with nothing', function () {
+            assert.throws(() => new Vector(), TypeError);
+        });
+        it('should fail to construct with null', function () {
+            assert.throws(() => new Vector(null, null), TypeError);
+        });
+        it('should fail to construct with undefined', function () {
+            assert.throws(() => new Vector(undefined, undefined), TypeError);
+        });
+        it('should fail to construct with infinity value', function () {
+            assert.throws(() => new Vector(Infinity, 10), TypeError);
+            assert.throws(() => new Vector(-Infinity, 10), TypeError);
+        });
+        it('should fail to add with numbers', function () {
+            assert.throws(() => vectorA.add(10), TypeError);
+        });
+        it('should fail to add with "own made object"', function () {
+           let object = {
+               pos: {
+                   x: 10,
+                   y: 5
+               }
+           }
+           assert.throws(() => vectorA.add(object), TypeError);
+        });
     });
 
     describe('Static', function () {
@@ -72,6 +109,11 @@ describe('Vectors', function () {
         it('normalize a vector', function () {
             let length = (Vector.normalize(vectorA)).magnitude();
             assert.strictEqual(length, 0.9999999999999999);
+        });
+
+        it('should be an angle', function () {
+            let degrees = Vector.directionAngle(vectorA);
+            assert.strictEqual(Math.round(degrees), 63);
         });
     });
 });
--- canvas/1-simple-game-loop/vector.js
+++ canvas/1-simple-game-loop/vector.js
@@ -1,3 +1,5 @@
+'use strict';
+
 module.exports = class Vector {
     pos = {
         x: null,
@@ -5,26 +7,39 @@ module.exports = class Vector {
     };
 
     constructor(x, y) {
+        if (Vector.#isNotANumber(x) || Vector.#isNotANumber(y)) throw new TypeError(`Got x: ${x.constructor} and y: ${y.constructor}. Wanted Number`);
         this.pos = {
             x: x,
             y: y
         }
     }
 
+    static #isNotANumber(value) {
+        return isNaN(value)  || value == undefined  || value == Infinity || value == -Infinity;
+    }
+
+    static #isVector(object) {
+        return object.constructor === Vector;
+    }
+
     add (vector) {
+        if (!Vector.#isVector(vector)) throw new TypeError(`Got ${vector.constructor} but wanted Vector object!`);
         this.pos = Vector.add(this, vector).pos;
     }
 
     static add(vector1, vector2) {
+        if (!Vector.#isVector(vector1) || !Vector.#isVector(vector2)) throw new TypeError(`Got ${vector1.constructor}  and ${vector2.constructor} but wanted Vector object!`);
         return new Vector(vector1.pos.x + vector2.pos.x,
             vector1.pos.y + vector2.pos.y);
     }
 
     subtract (vector) {
+        if (!Vector.#isVector(vector)) throw new TypeError(`Got ${vector.constructor} but wanted Vector object!`);
         this.pos = Vector.subtract(this, vector).pos;
     }
 
     static subtract(vector1, vector2) {
+        if (!Vector.#isVector(vector1) || !Vector.#isVector(vector2)) throw new TypeError(`Got ${vector1.constructor}  and ${vector2.constructor} but wanted Vector object!`);
         return new Vector(vector1.pos.x - vector2.pos.x,
             vector1.pos.y - vector2.pos.y);
     }
@@ -34,27 +49,32 @@ module.exports = class Vector {
     }
 
     static magnitude(vector) {
+        if (!Vector.#isVector(vector)) throw new TypeError(`Got ${vector.constructor} but wanted Vector object!`);
         return Math.sqrt(Math.pow(vector.pos.x, 2) + Math.pow(vector.pos.y, 2));
     }
 
     scalar(scalarValue) {
+        if (isNaN(scalarValue)) throw new TypeError(`Got ${scalarValue.constructor} but wanted Number!`);
         Vector.scalar(this, scalarValue);
     }
 
     static scalar(vector, scalarValue) {
+        if (isNaN(scalarValue) || !Vector.#isVector(vector)) throw new TypeError(`Got ${scalarValue.constructor} and wanted Number. Got ${vector.constructor} and wanted Vector object!`);
         for (let pos in vector.pos) {
             if (vector.pos.hasOwnProperty(pos)) {
                 vector.pos[pos] *= scalarValue;
             }
         }
-        return vector
+        return vector;
     }
 
     distance(vector) {
+        if (!Vector.#isVector(vector)) throw new TypeError(`Got ${vector.constructor} but wanted Vector object!`);
         return Vector.distance(this, vector);
     }
 
     static distance(vector1, vector2) {
+        if (!Vector.#isVector(vector1) || !Vector.#isVector(vector2)) throw new TypeError(`Got ${vector1.constructor}  and ${vector2.constructor} but wanted Vector object!`);
         return Math.sqrt(Math.pow(vector2.pos.x - vector1.pos.x,2)+ Math.pow(vector2.pos.y - vector1.pos.y,2));
     }
 
@@ -63,6 +83,7 @@ module.exports = class Vector {
     }
 
     static normalize(vector) {
+        if (!Vector.#isVector(vector)) throw new TypeError(`Got ${vector.constructor} but wanted Vector object!`);
         let length = vector.magnitude();
         for (let pos in vector.pos) {
             if (vector.pos.hasOwnProperty(pos)) {
@@ -70,5 +91,14 @@ module.exports = class Vector {
             }
         }
         return vector;
+    }
+
+    directionAngle () {
+        return Vector.directionAngle(this);
+    }
+
+    static directionAngle(vector) {
+        if (!Vector.#isVector(vector)) throw new TypeError(`Got ${vector.constructor} but wanted Vector object!`);
+        return Math.atan(vector.pos.y/vector.pos.x) * 180 /Math.PI;
     }
 }
